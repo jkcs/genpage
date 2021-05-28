05d8d7591272ae40a0ae60fba5bad7c2745f89ff
liuwei <liuwei@woaizuji.com>
2021-05-28 17:25:20 +0800

    :tada: 准备修改项目结构

--- packages/genpage-aliment/src/alert/alert.tsx
+++ packages/genpage-aliment/src/alert/alert.tsx
@@ -1,10 +1,15 @@
 import { VNode } from 'vue'
+import card from '@/card'
 
 export default {
+  components: {
+    card
+  },
   render(): VNode {
     return (
       <div>
         11111111111111
+        <card>2222222</card>
       </div>
     )
   }
