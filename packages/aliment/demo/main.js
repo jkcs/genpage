c048b8759e7179478d16a680044a4c3ec3c3f026
liuwei <liuwei@woaizuji.com>
2021-06-25 16:13:48 +0800

    :tada: 更换至sass

--- packages/aliment/demo/main.js
+++ packages/aliment/demo/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Button from '@/button'
-import '@/button/index.less'
+import '@/button/index.scss'
 
 const app = createApp(App)
 
