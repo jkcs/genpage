70b087648b658a07e60273e295e956a63ca34f9e
liuwei <liuwei@woaizuji.com>
2021-07-02 18:34:08 +0800

    :tada: 商品测试组件

--- packages/aliment/src/goods/goods.tsx
+++ packages/aliment/src/goods/goods.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, SetupContext } from 'vue'
-import { createBEM } from '@/utils'
+import { createBEM, isDef } from '@/utils'
 
 const { name, bem } = createBEM('goods')
 
@@ -39,17 +39,28 @@ export default defineComponent({
   },
 
   setup (props, ctx: SetupContext) {
-    const renderImg = () => (
-      props.src && <img class={ bem('img') } alt={ name } src={ props.src } />
+    const renderImg = () => props.src && (
+      <img class={ bem('img') } alt={ name } src={ props.src } />
     )
 
-    const renderPrice = () => props.amount && (
-      <div class={ bem('price') }>
-        <span class={ bem('price-prefix') }>{ props.prefix }</span>
-        <span class={ bem('price-amount') }>{ props.amount }</span>
-        <span class={ bem('price-unit') }>{ props.unit }</span>
-      </div>
-    )
+    const renderPrice = () => {
+      if (!isDef(props.amount)) {
+        return
+      }
+
+      const [_amount, __amount] = String(props.amount || 0).split('.')
+
+      return (
+        <div class={ bem('price') }>
+          <span class={ bem('price-prefix') }>{ props.prefix }</span>
+          <span class={ bem('price-amount') }>
+            <span>{ isDef(_amount) ? _amount : '' }</span>
+            <span>{ isDef(__amount) ? __amount : '' }</span>
+          </span>
+          <span class={ bem('price-unit') }>{ props.unit }</span>
+        </div>
+      )
+    }
 
     return () => (
       <div class={ bem([props.size]) }>
