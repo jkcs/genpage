368e917136338d9b481a68f328925eb05c0a3479
liuwei <liuwei@woaizuji.com>
2021-07-30 17:54:59 +0800

    :tada: aliment的button组件

--- packages/aliment/src/button/button.tsx
+++ packages/aliment/src/button/button.tsx
@@ -1,22 +1,38 @@
 import { defineComponent, PropType, SetupContext } from 'vue'
-import { createBEM, BUTTON_TYPE } from '../utils'
+import { createBEM, ComponentSize } from '../utils'
 
 const { name, bem } = createBEM('button')
+
+export type ButtonType = 'primary' | 'default' | 'striking' | 'text'
 
 export default defineComponent({
   name,
 
   props: {
+    size: {
+      type: String as PropType<ComponentSize>,
+      default: 'medium'
+    },
+
     type: {
-      type: String as PropType<BUTTON_TYPE>,
+      type: String as PropType<ButtonType>,
       default: 'default'
     },
 
+    radius: [Number, String],
+
+    disabled: Boolean
   },
 
   setup (props, ctx: SetupContext) {
+    const onCLick = (event?: Event) => {
+      if (!props.disabled) {
+        ctx.emit('click', event)
+      }
+    }
+
     return () => (
-      <div class={ bem([props.type]) }>
+      <div class={ bem([props.type, props.size]) } onClick={onCLick}>
         { ctx.slots.default?.() }
       </div>
     )
--- packages/aliment/src/utils/constant.ts
+++ packages/aliment/src/utils/constant.ts
@@ -1,10 +1,4 @@
-export enum BUTTON_TYPE {
-  PRIMARY = 'primary',
-  DEFAULT = 'default'
-}
-
 export default {
   PREFIX: 'gen',
-  SEPARATOR: '-',
-  BUTTON_TYPE
+  SEPARATOR: '-'
 }
