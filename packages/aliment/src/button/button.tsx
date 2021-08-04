01e1da0a3ec38bb55560ac4f969cba5f73dbd893
liuwei <liuwei@woaizuji.com>
2021-08-04 18:14:22 +0800

    :tada: 对button组件的测试用例

--- packages/aliment/src/button/button.tsx
+++ packages/aliment/src/button/button.tsx
@@ -1,12 +1,18 @@
-import { defineComponent, PropType, SetupContext } from 'vue'
-import { createBEM, ComponentSize } from '../utils'
+import { CSSProperties, defineComponent, PropType, SetupContext } from 'vue'
+import { createBEM, ComponentSize, isDef } from '../utils'
+import { addUnit } from '../utils/format/unit'
 
-const { name, bem } = createBEM('button')
+const {
+  name,
+  bem
+} = createBEM('button')
 
 export type ButtonType = 'primary' | 'default' | 'striking' | 'text'
 
 export default defineComponent({
   name,
+
+  emits: ['click'],
 
   props: {
     size: {
@@ -25,15 +31,20 @@ export default defineComponent({
   },
 
   setup (props, ctx: SetupContext) {
-    const onCLick = (event?: Event) => {
+    const onCLick = (event: Event) => {
       if (!props.disabled) {
         ctx.emit('click', event)
       }
     }
+    const style: CSSProperties = {}
+
+    if (isDef(props.radius)) {
+      style.borderRadius = addUnit(props.radius)
+    }
 
     return () => (
-      <div class={ bem([props.type, props.size]) } onClick={onCLick}>
-        { ctx.slots.default?.() }
+      <div class={bem([props.type, props.size])} style={style} onClick={onCLick}>
+        {ctx.slots.default?.()}
       </div>
     )
   }
---
+++ packages/aliment/tests/unit/button.spec.js
@@ -0,0 +1,71 @@
+import { mount } from '@vue/test-utils'
+import button from '@/button'
+import { createBEM } from '@/utils'
+
+describe('goods', () => {
+  const { name, bem } = createBEM('button')
+
+  it('renders slot when passed', () => {
+    const props = {
+      size: 'mini',
+      type: 'primary',
+      radius: 10
+    }
+    const wrapper = mount(button, {
+      propsData: props,
+      slots: {
+        default: 'button'
+      }
+    })
+    const element = wrapper.element
+
+    expect(element.innerHTML).toBe('button')
+  })
+
+  // size type radius
+  it('renders props size,type,radius when passed', () => {
+    const props = {
+      size: 'mini',
+      type: 'primary',
+      radius: 10
+    }
+    const wrapper = mount(button, { propsData: props })
+    const element = wrapper.element
+    const classes = wrapper.classes()
+    // size
+    expect(classes).toContain(bem([props.size]).split(' ')[1])
+    // type
+    expect(classes).toContain(bem([props.type]).split(' ')[1])
+    // radius
+    expect(element.style.borderRadius).toEqual(props.radius + 'px')
+  })
+
+  it('renders prop disabled when passed', async () => {
+    const props = {
+      disabled: true
+    }
+    const component = {
+      template: `<gen-button :disabled="disabled" @click="handleClick"/>`,
+      props: {
+        disabled: Boolean
+      },
+      components: {
+        'gen-button': button
+      },
+      methods: {
+        handleClick() {
+          this.$emit('TestClick')
+        }
+      }
+    }
+    const wrapper = mount(component, {
+      propsData: props,
+    })
+    await wrapper.trigger('click')
+    expect(wrapper.emitted().TestClick).not.toBeDefined()
+
+    await wrapper.setProps({ disabled: false })
+    await wrapper.trigger('click')
+    expect(wrapper.emitted().TestClick?.length).toBe(1)
+  })
+})
