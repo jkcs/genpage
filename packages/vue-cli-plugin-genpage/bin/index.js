369b00e8878b1746cd20811a6db1c6badd601b61
lw <1778768609@qq.com>
2021-08-13 22:09:59 +0800

    :tada: add create component commander

--- packages/vue-cli-plugin-genpage/bin/index.js
+++ packages/vue-cli-plugin-genpage/bin/index.js
@@ -1,27 +1,36 @@
 #!/usr/bin/env node
 'use strict'
 
+const { PACKAGE_JSON } = require('../src/util/build/constant')
 const inquirer = require('inquirer')
 const { createComponent } = require('./template/index')
+const commander = require('commander')
+const packageObj = require(PACKAGE_JSON)
 
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'type',
-    message: 'choose component type',
-    choices: [
-      'normal', 'functional'
-    ]
-  },
-  {
-    type: 'input',
-    name: 'name',
-    message: 'input component name'
-  }
-])
-  .then(answers => {
-    createComponent(
-      answers.name,
-      answers.type === 'functional'
-    )
+// const commander = new Commander()
+
+commander.version(`@genpage/vue-cli-plugin-genpage ${packageObj.version}`)
+
+commander.command('component [name]')
+  .description('create a new component')
+  .action(function (name) {
+    if (!!name) {
+      createComponent(name)
+      return
+    }
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name:'
+      }
+    ])
+      .then(answers => {
+        createComponent(answers.name)
+      })
+      .catch(() => {
+        console.log('get component name error')
+      })
   })
+
+commander.parse()
--- packages/vue-cli-plugin-genpage/bin/template/index.js
+++ packages/vue-cli-plugin-genpage/bin/template/index.js
@@ -1,54 +1,62 @@
-const { existsSync, emptyDirSync } = require('fs-extra')
+const {
+  existsSync,
+  emptyDirSync
+} = require('fs-extra')
 const { smartOutputFile } = require('../../src/util/build/fs')
 const { SRC_DIR } = require('../../src/util/build/constant')
 const { join } = require('path')
 const { chalk } = require('@vue/cli-shared-utils')
+const { camelize } = require('../../src/util/build')
 
-function createJS(name, isFunctional) {
-  return`import { createBEM, getName } from '@/utils/create'
+function createTSX (name) {
+  return `import { defineComponent, SetupContext } from 'vue'
+import { createBEM } from '../utils'
 
-const name = getName('${name}')
-const bem = createBEM(name)
-
-const props = {
-}
-
-export default {
+const {
   name,
-  ${ isFunctional ? 'functional: true,' : '' }
-  props,
-  
-  render () {
+  bem
+} = createBEM('${name}')
+
+export default defineComponent({
+  name,
+
+  props: {},
+
+  setup (props, ctx: SetupContext) {
     return (
-      <div class={ [bem()] }>
-      
+      <div class={bem()}>
+
       </div>
     )
   }
-}
+})
 `
 }
 
-function createLess() {
-  return `@import '../style/var';
+function createScss () {
+  return `@import '../style/var';`
+}
+
+function createInstall (name) {
+  const upperCaseName = camelize(name, true)
+  return `import type { App } from 'vue'
+import ${upperCaseName} from './${name}'
+import { WithAPPInstall } from '../utils'
+
+${upperCaseName}.install = function (app: App) {
+  app.component(${upperCaseName}.name, ${upperCaseName})
+}
+
+export { ${upperCaseName} }
+export default ${upperCaseName} as WithAPPInstall<typeof ${upperCaseName}>
 `
 }
 
-function createInstall(name) {
-  return `import ${name} from './${name}'
-
-${name}.install = function (Vue) {
-  Vue.component(${name}.name, ${name})
-}
-export default ${name}
-`
-}
-
-function createComponent(name, isFunctional) {
+function createComponent (name) {
   const path = join(SRC_DIR, name)
-  const jsFile = join(path, `${name}.js`)
-  const lessFile = join(path, `index.less`)
-  const entryFile = join(path, `index.js`)
+  const tsxFile = join(path, `${name}.tsx`)
+  const scssFile = join(path, `index.scss`)
+  const entryFile = join(path, `index.ts`)
 
   if (existsSync(path)) {
     console.log(`${chalk.yellow(name)} is exists. please delete it then try again`)
@@ -56,8 +64,8 @@ function createComponent(name, isFunctional) {
   }
 
   emptyDirSync(path)
-  smartOutputFile(jsFile, createJS(name, isFunctional))
-  smartOutputFile(lessFile, createLess(name))
+  smartOutputFile(tsxFile, createTSX(name))
+  smartOutputFile(scssFile, createScss(name))
   smartOutputFile(entryFile, createInstall(name))
 
   console.log(`${chalk.cyan(name)} component is created.`)
